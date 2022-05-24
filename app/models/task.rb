f7c3b4a0a6dd78505515a29a2fc485a11c24fff7
zidajoseph <zidajoseph60@gmail.com>
2022-05-24 12:53:39 +0000

    adding sort, search and pagination

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,4 +2,7 @@ class Task < ApplicationRecord
     validates :name, :content,  presence: true
     enum status: { notstarted:1,inprogress:2,completed:3 }
     enum priority: { high:1,medium:2,low:3 }
+    scope :search_name, -> name {where("name LIKE ?", "%#{name}%")}
+    scope :search_status, -> status {where(status: status)}
+    paginates_per 2
 end
